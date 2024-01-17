b3e87a642d6deed2bb4e61f17306f1c8a26c8c47
JJ-Bas <jef.j.bas@gmail.com>
2024-01-17 21:00:53 +0100

    string name

---
+++ src/Name.java
@@ -0,0 +1,7 @@
+public class Name {
+    public static void main(String[] args) {
+        String name = "Jef Bas";
+        int age = 34;
+        System.out.println(name + " is " + age + " years old ");
+    }
+}
