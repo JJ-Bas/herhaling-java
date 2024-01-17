1efa6ed8a0116ed76c4b80aa15196a48f9285e4b
JJ-Bas <jef.j.bas@gmail.com>
2024-01-17 21:13:07 +0100

    datatypes

---
+++ src/Datatypes.java
@@ -0,0 +1,9 @@
+public class Datatypes {
+    public static void main(String[]args){
+        byte voorbeeldByte = 127;
+        short vbShort = 32767;
+        long vbLong = 2147483647;
+
+        System.out.println( voorbeeldByte + " " + vbShort + " " + vbLong);
+    }
+}
